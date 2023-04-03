d2e127015e29a2027e46a2186cae168b40dafa29
phamquyet9688 <117346649+phamquyet9688@users.noreply.github.com>
2023-04-03 17:10:50 +0700

    fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,6 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <div className='nav' >
-            <h1 className='h1logo'>NETFLIX</h1>
-            <div>
             <div className="logo">
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
